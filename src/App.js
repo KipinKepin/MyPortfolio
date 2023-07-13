b4f9fdae856e4f7f269968d8687380e7a49a9521
KipinKepin <kipinnn873@gmail.com>
2023-07-13 12:53:28 +0700

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import Home from "./router/Home";
+import About from "./router/About";
+import Project from "./router/Project";
+import Contact from "./router/Contact";
+import { Routes, Route } from "react-router-dom";
+import ComingSoon from "./components/ComingSoon";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/soon" element={<ComingSoon />} />
+      </Routes>
+    </>
   );
 }
 
